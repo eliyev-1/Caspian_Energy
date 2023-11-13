75d75cbebacd1fe62c2f5994157bff5198d3f824
Ali <designman699@gmil.com>
2023-11-13 17:22:18 +0400

    navbar hazirdi resposivlik qalib

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Layout from './components/layout'
 import Home from './pages/Home'
+import About from './pages/Home/About'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Layout>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
       </Routes>
     </Layout>
 
--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import "./layout.css"
+ import { Link } from 'react-router-dom'
 import NavLogo from "../../assets/img/navLogo.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
-const Layout = () => {
+const Layout = ({children}) => {
   return (
     <>
     <header>
@@ -26,10 +27,35 @@ const Layout = () => {
                         </div>
 
                   </div>
+                  <div className="nav__right__bottom">
+                        
+                              <ul className='nav__right__bottom__menu'>
+                                    <li> <Link to='/' className='menu__link'>Home</Link> </li>
+                                    <li> <Link to='/about'className='menu__link'>About</Link> </li>
+                                    <li> <Link to='/'className='menu__link'>Products</Link> </li>
+                                    <li> <Link to='/'className='menu__link'>Partners</Link> </li>
+                                    <li> <Link to='/'className='menu__link'>Catalogue</Link> </li>
+                                    <li> <Link to='/'className='menu__link'>News</Link> </li>
+                                    <li> <Link to='/'className='menu__link'>Contact</Link> </li>
+                                    
+                              </ul>
+
+                              <select className='nav__right__bottom__language'>
+                                    <option value=""><Link to='/'className='menu__link'>AZE</Link></option>
+                                    <option value=""><Link to='/'className='menu__link'>ENG</Link></option>
+                                    <option value=""><Link to='/'className='menu__link'>RUS</Link></option>
+                              </select>
+
+                   
+                        
+                  </div>
 
             </div>
       </nav>
     </header>
+    <div className="container">
+    {children}
+    </div>
     </>
   )
 }
