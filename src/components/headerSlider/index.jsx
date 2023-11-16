252c9124dac605857abfc35e30932993b689be33
eliyev-1 <designman699@gmail.com>
2023-11-16 20:04:13 +0400

    save

--- src/components/headerSlider/index.jsx
+++ src/components/headerSlider/index.jsx
@@ -1,41 +1,67 @@
-import React from 'react'
-import Slider from 'react-slick';
+import React from "react";
+import Slider from "react-slick";
+import "./headerSlider.css";
 
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import img1 from "../../assets/img/headerSlider/img1.jpg"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import img1 from "../../assets/img/headerSlider/img1.jpg";
+import { Link } from "react-router-dom";
 
 const headerSlider = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows:false
-      };
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    className: "header__slider__item__img",
+  };
+
   return (
     <>
-    <Slider  className="header__slider" {...settings}>
-    <div>
-        <img src={img1} alt="" />
-        
-      </div>
-      <div>
-        <img src={img1} alt="" />
-        
-      </div>
-      <div>
-        <img src={img1} alt="" />
-        
-      </div>
-     
-     
+      <Slider className="header__slider" {...settings}>
+        <div className="header__slider__item">
+          <div
+            className="header__slider__item__img"
+            style={{ backgroundImage: `url(${img1})` }}
+          ></div>
 
-    </Slider>
-    
+          <div className="container">
+            <h1>
+              Hər növ izolyasiya və tikinti materiallarının topdan və pərakəndə
+              satışı
+            </h1>
+            <div className="learn__more__box">
+              <Link to="/" className="learn__more__link link">
+                Ətrafli
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="header__slider__item">
+          <div
+            className="header__slider__item__img"
+            style={{ backgroundImage: `url(${img1})` }}
+          ></div>
+
+          <div className="container">
+            <h1>
+              Hər növ izolyasiya və tikinti materiallarının topdan və pərakəndə
+              satışı
+            </h1>
+            <div className="learn__more__box">
+              <Link to="/" className="learn__more__link link">
+                Ətrafli
+              </Link>
+            </div>
+          </div>
+        </div>
+      </Slider>
     </>
-  )
-}
+  );
+};
 
-export default headerSlider
+export default headerSlider;
--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,89 +1,280 @@
-import React from 'react'
+import React, { useState } from "react";
 import "./layout.css";
- import { Link } from 'react-router-dom';
+import { Link ,useLocation } from "react-router-dom";
 import NavLogo from "../../assets/img/navLogo.png";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhone,faBars,faX } from '@fortawesome/free-solid-svg-icons';
-import HeaderSlider from '../headerSlider';
-const Layout = ({children}) => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faBars, faX } from "@fortawesome/free-solid-svg-icons";
+import HeaderSlider from "../HeaderSlider";
+const Layout = ({ children }) => {
+  let [navRightBottom, setnavRightBottom] = useState("nav__right__bottom");
+  let [menuButton, setMenuButton] = useState("nav__menu__button");
+  let [exitButton, setExitButton] = useState("nav__exit__button");
+  let [navLogo, setNavLogo] = useState("nav__logo");
+  let [nav, setNav] = useState("");
+  let menuClick = () => {
+    setnavRightBottom(
+      navRightBottom === "nav__right__bottom"
+        ? "nav__right__bottom__active "
+        : "nav__right__bottom"
+    );
+    setMenuButton(
+      menuButton === "nav__menu__button"
+        ? "nav__menu__button__active"
+        : "nav__menu__button"
+    );
+    setExitButton(
+      exitButton === "nav__exit__button"
+        ? "toggle__active"
+        : "nav__exit__button"
+    );
+    setNav(nav === "" ? "nav__active" : "");
+    setNavLogo(navLogo === "nav__logo" ? "nav__logo__active" : "nav__logo");
+  };
+  let closeMenu = () => {
+      setnavRightBottom("nav__right__bottom");
+      setMenuButton("nav__menu__button");
+      setExitButton("nav__exit__button");
+      setNav("");
+      setNavLogo("nav__logo");
+    };
+    const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
   return (
     <>
-    <header>
-      <nav className='container'>
+      <header className=".header">
+        <nav className={`nav ${isHomePage ? "" : "static-navbar"}`}>
+          <div className="container">
             <div className="nav__left">
-                 
-                  <div className="nav__logo">
-                        <img src={NavLogo} alt="" />
-                  </div>
-                  <div className="nav__buttons">
-                        <div className="nav__menu__button">
-                        <FontAwesomeIcon icon={faBars} />
-
-                        </div>
-                        <div className="nav__exit__button">
-                        <FontAwesomeIcon icon={faX} />
-
-                        </div>
-
-
-                
-                  </div>
-
-                 
+              <div className={navLogo}>
+              <Link to="/" className="link" onClick={closeMenu}>
+              <img src={NavLogo} alt="" />
+              </Link>{" "}
+              </div>
+              <div className="nav__buttons" onClick={menuClick}>
+                <div className={menuButton}>
+                  <FontAwesomeIcon icon={faBars} />
+                </div>
+                <div className={exitButton}>
+                  <FontAwesomeIcon icon={faX} />
+                </div>
+              </div>
             </div>
             <div className="nav__right">
-                  <div className="nav__right__top">
-                        <div className="nav__right__top__date">
-                        Monday – Saturday 9:00 – 18:00
-                        </div>
-                        <div className="nav__right__top__numbers">
-                        <span><FontAwesomeIcon icon={faPhone} bounce /></span>
-                         <span> +994125555000 </span><span> +994504588888  </span>
-                        </div>
+              <div className="nav__right__top">
+                <div className="nav__right__top__date">
+                  Monday – Saturday 9:00 – 18:00
+                </div>
+                <div className="nav__right__top__numbers">
+                  <span>
+                    <FontAwesomeIcon icon={faPhone} bounce />
+                  </span>
+                  <span> +994125555000 </span>
+                  <span> +994504588888 </span>
+                </div>
+              </div>
+              <div className={navRightBottom}>
+                <ul className="nav__right__bottom__menu">
+                <li>
+              {" "}
+              <Link to="/" className="link" onClick={closeMenu}>
+                Home
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/about" className="link" onClick={closeMenu}>
+                About
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/" className="link" onClick={closeMenu}>
+              Products
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/about" className="link" onClick={closeMenu}>
+              Partners
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/" className="link" onClick={closeMenu}>
+              Catalogue
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/about" className="link" onClick={closeMenu}>
+              News
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/" className="link" onClick={closeMenu}>
+              Contact
+              </Link>{" "}
+            </li>
+           
+                </ul>
 
-                  </div>
-                  <div className="nav__right__bottom">
-                        
-                              <ul className='nav__right__bottom__menu'>
-                                    <li> <Link to='/' className='menu__link'>Home</Link> </li>
-                                    <li> <Link to='/about'className='menu__link'>About</Link> </li>
-                                    <li> <Link to='/'className='menu__link'>Products</Link> </li>
-                                    <li> <Link to='/'className='menu__link'>Partners</Link> </li>
-                                    <li> <Link to='/'className='menu__link'>Catalogue</Link> </li>
-                                    <li> <Link to='/'className='menu__link'>News</Link> </li>
-                                    <li> <Link to='/'className='menu__link'>Contact</Link> </li>
-                                    
-                              </ul>
-
-                              <select className='nav__right__bottom__language'>
-                                    <option value=""><Link to='/'className='menu__link'>AZE</Link></option>
-                                    <option value=""><Link to='/'className='menu__link'>ENG</Link></option>
-                                    <option value=""><Link to='/'className='menu__link'>RUS</Link></option>
-                              </select>
-                              <ul className='nav__right__bottom__slider__menu__laguage'>
-                                    <li> <Link to='/' className='menu__link'>AZE</Link> </li>
-                                    <li> <Link to='/about'className='menu__link'>ENG</Link> </li>
-                                    <li> <Link to='/'className='menu__link'>RUS</Link> </li>
-                                   
-                                    
-                              </ul>
-
-
-                   
-                        
-                  </div>
-
+                <select className="nav__right__bottom__language">
+                  <option value="">
+                    <Link to="/" className="link" >
+                      AZE
+                    </Link>
+                  </option>
+                  <option value="">
+                    <Link to="/" className="link">
+                      ENG
+                    </Link>
+                  </option>
+                  <option value="">
+                    <Link to="/" className="link">
+                      RUS
+                    </Link>
+                  </option>
+                </select>
+                <ul className="nav__right__bottom__slider__menu__laguage">
+                  <li>
+                    {" "}
+                    <Link to="/" className="link" onClick={closeMenu}>
+                      AZE
+                    </Link>{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    <Link to="/about" className="link" onClick={closeMenu}>
+                      ENG
+                    </Link>{" "}
+                  </li>
+                  <li>
+                    {" "}
+                    <Link to="/" className="link" onClick={closeMenu}>
+                      RUS
+                    </Link>{" "}
+                  </li>
+                </ul>
+              </div>
             </div>
-      </nav>
-      <div className="header__slider">
-            <HeaderSlider />
-      </div>
-    </header>
-    <div className="container">
-    {children}
-    </div>
-    </>
-  )
-}
+          </div>
+        </nav>
 
-export default Layout
+        {isHomePage && (
+          <div className="header__slider">
+            <HeaderSlider />
+          </div>
+        )}
+      </header>
+      <div className="container">{children}</div>
+
+      <footer className="container">
+      <div className="footer__logo">
+              <Link to="/" className="link" onClick={closeMenu}>
+              <img src={NavLogo} alt="" />
+              </Link>{" "}
+              </div>
+              <ul className="footer__menu">
+                <li>
+              {" "}
+              <Link to="/" className="link" onClick={closeMenu}>
+                Home
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/about" className="link" onClick={closeMenu}>
+                About
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/" className="link" onClick={closeMenu}>
+              Products
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/about" className="link" onClick={closeMenu}>
+              Partners
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/" className="link" onClick={closeMenu}>
+              Catalogue
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/about" className="link" onClick={closeMenu}>
+              News
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/" className="link" onClick={closeMenu}>
+              Contact
+              </Link>{" "}
+
+              
+            </li>
+            <li>
+              {" "}
+              <Link to="/" className="link" onClick={closeMenu}>
+              Support
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/about" className="link" onClick={closeMenu}>
+              Privacy policy
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/" className="link" onClick={closeMenu}>
+              Terms of use
+              </Link>{" "}
+            </li>
+           
+                </ul>
+
+                <div className="footer__bottom">
+                  <p>@All right reserved.Developed by Webcoder.</p>
+                  <ul className="footer__bottom__left">
+                <li>
+              {" "}
+              <Link to="/" className="link" onClick={closeMenu}>
+              Support
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/about" className="link" onClick={closeMenu}>
+              Privacy policy
+              </Link>{" "}
+            </li>
+            <li>
+              {" "}
+              <Link to="/" className="link" onClick={closeMenu}>
+              Terms of use
+              </Link>{" "}
+            </li>
+            
+            
+           
+                </ul>
+
+                </div>
+
+
+        
+      </footer>
+    </>
+  );
+};
+
+export default Layout;
